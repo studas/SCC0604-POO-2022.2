2f039ce23cc490b106c1d009956ff11cc3f68fad
Matheus Cirillo <39923113+cirillom@users.noreply.github.com>
2022-08-30 14:32:36 +0000

    iniciado codigo do ex2 da lista 1

---
+++ lista01/ex02/java_project/Rede.java
@@ -0,0 +1,3 @@
+class Rede extends Dispositivo {
+
+}
---
+++ lista01/ex02/java_project/Tela.java
@@ -0,0 +1,7 @@
+class Tela extends Dispositivo {
+  private float brilho;
+
+  public void alteraBrilhoDeExibicao(float novo_brilho){
+    brilho = novo_brilho;
+  }
+}
