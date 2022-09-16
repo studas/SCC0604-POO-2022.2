6a80ffa9ca23ba0cdc4e1b663080acddecb6b077
cirillom <matheuscirillo@usp.br>
2022-09-16 08:41:09 -0300

    Commit inicial lista03/ex03

---
+++ lista03/lista03-ex03/src/main/java/com/scc0604/lista03/ex03/Lista03Ex03.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.scc0604.lista03.ex03;
+
+/**
+ *
+ * @author cillor
+ */
+public class Lista03Ex03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
