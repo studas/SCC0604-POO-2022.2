b44f13eea4e8b1942986559d2ebc10ce94fb1e50
cirillom <matheuscirillo@usp.br>
2022-09-17 16:46:23 -0300

    removed javadoc

--- lista02/lista02-ex03/src/main/java/com/scc0604/lista02/ex03/Main.java
+++ lista02/lista02-ex03/src/main/java/com/scc0604/lista02/ex03/Main.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.scc0604.lista02.ex03;
 
-/**
- *
- * @author 12547750
- */
 public class Main {
 
     /**
--- lista02/lista02-ex03/src/main/java/com/scc0604/lista02/ex03/Personagem.java
+++ lista02/lista02-ex03/src/main/java/com/scc0604/lista02/ex03/Personagem.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.scc0604.lista02.ex03;
 
 import java.util.*;
 
-/**
- *
- * @author 12547750
- */
 public class Personagem {
     private String nome;
     private List<Superpoder> ataques = new ArrayList<Superpoder>();
