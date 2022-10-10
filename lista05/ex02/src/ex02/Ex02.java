d1b62fb54aeb1c5419dbe053d12b85ee326f7b2b
CarlosCraveiro <carlos.craveiro@usp.br>
2022-10-10 13:13:37 -0300

    Adiciona comentarios a Ex02.main

--- lista05/ex02/src/ex02/Ex02.java
+++ lista05/ex02/src/ex02/Ex02.java
@@ -3,10 +3,27 @@ package ex02;
 public class Ex02 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Cria classe MinhaMatrizDinamica
         MinhaMatrizDinamica ma = new MinhaMatrizDinamica(5, 5);
+        
+        // Imprime na tela matriz
+        System.out.println("Matriz inicializada com zeros:");
         System.out.print(ma);
+        
+        // Preenche a diagonal principal com o valor 42
+        for(int i = 0; i < 5; i++) {
+            ma.preenchePosicao(i, i, 42);
+        }
+        
+        // Imprime na tela matriz
+        System.out.println("Matriz com a diagonal principal preenchida:");
+        System.out.print(ma);
+        
+        // Preenche cada elemento da matriz com a soma dos indexes
         ma.preencheComSomaDeIndexes();
+        
+        // Imprime a matriz
+        System.out.println("Matriz com elementos = i + j");
         System.out.print(ma);
     }
     
