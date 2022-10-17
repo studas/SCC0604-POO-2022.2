64a5805bb2def20b8a5bd97847d95c95f8490c29
cirillom <matheuscirillo@usp.br>
2022-10-16 22:03:09 -0300

    lista05ex04 pronto

--- lista05/lista05ex04/lista05ex04/src/lista05ex04/Lista05ex04.java
+++ lista05/lista05ex04/lista05ex04/src/lista05ex04/Lista05ex04.java
@@ -1,4 +1,18 @@
-Explicação da estratégia aplicada para causar o erro java.lang.StackOverflowError:
+package lista05ex04;
+
+public class Lista05ex04 {
+
+    public static void main(String[] args) {
+        DoStackOverflow(1);
+        // TODO code application logic here
+    }
+    
+    public static void DoStackOverflow(int i) {
+        DoStackOverflow(i);
+    }
+    
+}
+/* Explicação da estratégia aplicada para causar o erro java.lang.StackOverflowError:
 
 
 Parâmetros e variáveis locais são alocados na stack. A stack tem uma quantidade fixa de memória, caso essa memória se esgote, um erro de StackOverflow acontecerá. No exercício foi pedido para que criássemos um código que propositalmente causasse o erro de stackOverflow. 
@@ -29,3 +43,5 @@ Exception in thread "main" java.lang.StackOverflowError
 	/home/cillor/.cache/netbeans/15/executor-snippets/run.xml:111: The following error occurred while executing this line:
 /home/cillor/.cache/netbeans/15/executor-snippets/run.xml:68: Java returned: 1
 BUILD FAILED (total time: 0 seconds)
+
+*/
