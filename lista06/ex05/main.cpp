e8d2cfc7123aeed33d12f62d131539736c88547b
GuScarenci <gustavomourascarenci@usp.br>
2022-10-28 22:38:22 -0300

    codigo melhorado

--- lista06/ex05/main.cpp
+++ lista06/ex05/main.cpp
@@ -21,7 +21,6 @@ int main(int argc, char** argv) {
         meuVetor.push_back(NumComplexo(rand()%100,rand()%100));
     }
     
-    
     NumComplexo *somaComplexo = new NumComplexo(0,0);
     
     std::vector<NumComplexo>::iterator ptr;//Iterator
@@ -30,15 +29,12 @@ int main(int argc, char** argv) {
         *somaComplexo = *somaComplexo + *ptr;
     }
     
-    NumComplexo *somaComplexo2 = new NumComplexo(0,0);
-    
     //Outro jeito de fazer um iterator
     /*for(auto& elemento : meuVetor){
-        *somaComplexo2 = *somaComplexo2 + elemento;
+        *somaComplexo = *somaComplexo + elemento;
     }*/
     
     std::cout << *somaComplexo << std::endl;
-    std::cout << *somaComplexo2;
     delete(somaComplexo);//Livra a memória deletando todos os objetos utilizados no programa.
     
     return 0;
