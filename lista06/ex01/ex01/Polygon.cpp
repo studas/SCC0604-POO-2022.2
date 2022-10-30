4d236ccc84d98d7e7baccc2c10e468f6de64628b
GuScarenci <gustavomourascarenci@usp.br>
2022-10-30 18:41:00 -0300

    ex-feito

--- lista06/ex01/ex01/Polygon.cpp
+++ lista06/ex01/ex01/Polygon.cpp
@@ -18,13 +18,11 @@ auto Polygon::area() -> float {
         x2 = points[i+1].getX();
         y2 = points[i+1].getY();
         _area += (x1*y2 - y1*x2);
-        std::cout << "S: " << (x1*y2) << "S2: " << (-y1 * x2) << std::endl;
     }
     x1 = points[nPoints-1].getX();
     y1 = points[nPoints-1].getY();
     x2 = points[0].getX();
     y2 = points[0].getY();
-        std::cout << "S: " << (x1*y2) << "S2: " << (-y1 * x2) << std::endl;
     _area += (x1*y2 - y1*x2);
     _area /= 2;
     if(_area < 0){
