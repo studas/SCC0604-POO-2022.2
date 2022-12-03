a7b22587de873f61ff17351521432e4aa45b43ed
CarlosCraveiro <carlos.craveiro@usp.br>
2022-12-03 14:57:06 -0300

    Inicia exercicio 02

---
+++ lista09/ex02/src/ex02/Ex02.java
@@ -0,0 +1,16 @@
+package ex02;
+
+/**
+ *
+ * @author coveiro
+ */
+public class Ex02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
